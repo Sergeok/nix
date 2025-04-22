b4f50b7c4a9993aeb90b6253386d1e0e0fce1718
SuminSV <suminsv@ssp-soft.com>
2025-04-22 22:19:25 +0700

    f

--- system-modules/system-x11.nix
+++ system-modules/system-x11.nix
@@ -9,6 +9,7 @@
   services.xserver = {
     enable = true;
     displayManager.lightdm.enable = true;
+    displayManager.defaultSession = "none+i3";
     windowManager.i3.enable = false; # важно, чтобы не было конфликта с home-manager
 	xkb = {
       layout = "us,ru";
