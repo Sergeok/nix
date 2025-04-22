fd6d860fc9c8b761f22864c64c36801f6340bff0
SuminSV <suminsv@ssp-soft.com>
2025-04-22 21:17:22 +0700

    d

--- home-modules/theme/nord/theme.nix
+++ home-modules/theme/nord/theme.nix
@@ -3,14 +3,14 @@
 {
   home.file = {
     # Шрифты и wallpaper
-    ".wallpaper/wallpaper.png".source = ../nord-dotfiles/assets/images/wallpaper.png;
-    ".local/share/fonts".source = ../nord-dotfiles/assets/fonts;
+    ".wallpaper/wallpaper.png".source = ../../../nord-dotfiles/assets/images/wallpaper.png;
+    ".local/share/fonts".source = ../../../nord-dotfiles/assets/fonts;
 
-    ".config/btop".source = /etc/nixos/nord-dotfiles/.config/btop;
-    ".config/cava".source = /etc/nixos/nord-dotfiles/.config/cava;
-    ".config/fastfetch".source = /etc/nixos/nord-dotfiles/.config/fastfetch;
-    ".config/harlequin".source = /etc/nixos/nord-dotfiles/.config/harlequin;
-    ".config/superfile".source = /etc/nixos/nord-dotfiles/.config/superfile;
+    ".config/btop".source = ../../../nord-dotfiles/.config/btop;
+    ".config/cava".source = ../../../nord-dotfiles/.config/cava;
+    ".config/fastfetch".source = ../../../nord-dotfiles/.config/fastfetch;
+    ".config/harlequin".source = ../../../nord-dotfiles/.config/harlequin;
+    ".config/superfile".source = ../../../nord-dotfiles/.config/superfile;
   };
   
   # Обновляем кеш шрифтов
@@ -21,7 +21,7 @@
   # =========================
   # Kitty Terminal
   # =========================
-  home.file.".config/kitty".source = ../nord-dotfiles/.config/kitty;
+  home.file.".config/kitty".source = ../../../nord-dotfiles/.config/kitty;
 
   # =========================
   # Starship Prompt
@@ -30,7 +30,7 @@
     enable = true;
     settings = {}; # Можно заменить на inline конфиг или файл
   };
-  home.file.".config/starship.toml".source = /etc/nixos/nord-dotfiles/.config/starship.toml;
+  home.file.".config/starship.toml".source = ../../../nord-dotfiles/.config/starship.toml;
 
   # =========================
   # Zsh + Oh-My-Zsh + Nord Theme
@@ -51,5 +51,5 @@
     '';
   };
   home.file.".oh-my-zsh/custom/themes/nord-deep/nord.zsh-theme".source =
-    /etc/nixos/nord-dotfiles/.config/nord.zsh-theme;
+    ../../../nord-dotfiles/.config/nord.zsh-theme;
 }
